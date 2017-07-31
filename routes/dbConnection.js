2257d9bd0a8f09728ad50c8bf50d4f6f01aa6765
vivek sharma <vsharma254@gmail.com>
2017-07-31 14:25:30 +0530

    change to mongo

--- routes/dbConnection.js
+++ routes/dbConnection.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-var mongodb = "mongodb://127.0.0.1/EmpMDB";
+//var mongodb = "mongodb://127.0.0.1/EmpMDB";
+var mongodb = "mongodb://FirstUser:viveksharma@ds155509.mlab.com:55509/empdb";
 mongoose.connect(mongodb);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb error in connection'));
